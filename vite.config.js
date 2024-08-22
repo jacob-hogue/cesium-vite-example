a4b04be2491b369b3228eed7a5d1ff9edf6493d0
Jacob H <35144268+jacob-hogue@users.noreply.github.com>
2024-08-22 09:58:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -26,12 +26,4 @@ export default defineConfig({
       ],
     }),
   ],
-  build: {
-    rollupOptions: {
-      external: ['protobufjs/dist/minimal/protobuf.js'],
-    },
-  },
-  esbuild: {
-    external: ['protobufjs/dist/minimal/protobuf.js'],
-  },
 });
